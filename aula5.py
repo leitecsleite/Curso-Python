a2e6e2aebec6ab028e694ab2110f6bb7081de9e9
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-08-07 14:55:46 -0300

    Operador lógico "AND"

---
+++ aula5.py
@@ -0,0 +1,13 @@
+#and (e) or (ou) not (não)
+
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '123456'
+
+#if true: 
+
+if entrada == 'E' and senha_digitada == senha_permitida:
+    print('Entrar')
+else: 
+    print('Sair')
