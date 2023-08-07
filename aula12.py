78899c275515e5ab48b3aeeae931df43c696b06d
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-08-07 16:55:42 -0300

    String

---
+++ aula12.py
@@ -0,0 +1,10 @@
+# Introdução ao try/except 
+# try -> tentar executar o código
+# except -> ocorreu algum erro ao tentar executar
+
+numero = input('Vou dobrar o número que vc digitou: ')
+
+numero_int = int(numero)
+print(f'O dobro de {numero} é {numero_int * 2}')
+
+#print(f'O dobro de {numero} é {numero_float * 2:.0f}')
