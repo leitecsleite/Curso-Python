87d91d406592c5b00938814cd66b9290c780512f
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-08-07 14:13:36 -0300

    formatar strings

---
+++ aula1.py
@@ -0,0 +1,15 @@
+nome = 'Lucas Leite'; 
+Altura = 1.70; 
+peso = 95; 
+imc = peso / Altura ** 2
+
+linha_1 = f'{nome} tem {Altura:.2} de altura'; 
+linha_2 = f'pesa {peso} quilos e seu IMC é'; 
+linha_3 = f'{imc:.2f}'; 
+
+
+print(linha_1); 
+print(linha_2); 
+print(linha_3)
+
+
