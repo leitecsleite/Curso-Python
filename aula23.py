af063dfd51d38e5608ad539dcbf2507aee1875e4
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-08-08 15:22:35 -0300

    def is even or is odd

--- aula23.py
+++ aula23.py
@@ -1,11 +1,17 @@
+def even_odd(number):
+    multiples_of_two = number % 2 == 0
 
-def par_impar(numero):
-    multiplos_de_dois = numero % 2 == 0
-
-    if multiplos_de_dois:
-        return f'{numero} é par'
+    if multiples_of_two:
+        return f'{number} is even'
     else: 
-        return f'{numero} é impar'
+        return f'{number} is odd'
 
-print(par_impar(2))
-print(par_impar(3))
+# Call the function with different numbers and print the results
+print(even_odd(2))  # Output: 2 is even
+print(even_odd(3))  # Output: 3 is odd
+
+
+
+
+
+
