e1308c3e35e292457ae103b917c5519bb907ee35
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-08-17 09:16:50 -0300

    List comprehension em Python

---
+++ aula39.py
@@ -0,0 +1,13 @@
+#List comprehension em Python 
+#List comprehension é uma forma rápida para criar listas 
+
+lista = []
+
+for numero in range(10):
+    lista.append(numero)
+
+print(lista)
+
+lista = [numero * 2 for numero in range(10)]
+
+print(lista)
