3760a3dea19357db8ec837335b4e376956ca0c92
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-08-17 11:47:43 -0300

    Data filtering in list comprehension (filter)

---
+++ aula41.py
@@ -0,0 +1,27 @@
+lista = []
+for numero in range(10):
+    lista.append(numero)
+# print(lista)
+
+lista = [
+    numero * 2
+    for numero in range(10)
+]
+# print(list(range(10)))
+# print(lista)
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 30, },
+]
+
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+    if ( produto['preco'] >= 20 and produto['preco'] * 1.05) > 10 
+]
+
+print (*novos_produtos, sep='\n')
